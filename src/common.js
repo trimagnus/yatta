c930698f941096b7b10c02fb04f70c92b707666e
trimagnus <tri_magnus@hotmail.com>
2022-03-03 12:42:04 -0800

    Fixed bug with invalid date input

--- src/common.js
+++ src/common.js
@@ -1,9 +1,11 @@
 export function convertFromDateInput(date) {
+  if(!date) return;
   let split = date.split('-');
   return `${split[1]}/${split[2]}/${split[0]}`;
 }
 
 export function convertToDateInput(date) {
+  if(!date) return;
   let split = date.split('/');
   if(split[2].length === 2) {
     split[2] = `20${split[2]}`;

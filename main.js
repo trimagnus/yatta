9aec36131a8781756261861816ae828aba9daacc
trimagnus <tri_magnus@hotmail.com>
2022-03-18 08:54:03 -0700

    Build for publishing demo

--- main.js
+++
@@ -1,8 +0,0 @@
-import './styles.css';
-import './favicon.svg';
-
-import storage from "./src/storage.js";
-import App from "./src/app.js";
-
-let appData = storage.retrieveData();
-document.querySelector('body').appendChild(new App(appData.projects));
